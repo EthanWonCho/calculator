f1caeca917bdd4e68fecb4229dcf2b814cc07409
Ethan Cho <ethanwonc@icloud.com>
2022-07-03 15:05:45 +0900

    new ideas

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+
 int main() {
-	cout << "give two integer, one operator:";
+	printf("give two integer, one operator: ");
 	int a, b;
 	char op;
-	cin >> a >> b >> op;
+	scanf("%d%d%c", &a, &b, &op);
 	switch (op) {
-	case'+':
-		cout << a + b << endl;
+	case '+':
+		printf("%d\n", a + b);
 		break;
-	case'-':
-		cout << a - b << endl;
+	case '-':
+		printf("%d\n", a - b);
 		break;
 	}
 }
+/*
+#include <stdio.h>
+int add(char* cmdptr){
+	char cmd[20][50];
+	for(int i = 0; i < 20; i++)
+		for(int j = 0; j < 50; j++)
+			cmd[i][j] = *(cmdptr + (i*50) + j)
+		}
+	printf("%s", cmd);
+}
+int main(){
+	char cmd[20][50];
+	//getting cmd
+	int i = 0, j = 0;
+	while(1){
+		scanf("%c", &cmd[i][j]);
+		if(cmd[i][j] == ' ') {i++;j=0;}
+		else if(cmd[i][j] == '\n'){break;}
+		else j++;
+	}
+	//calling functions
+	if(!(cmd[0][0])){
+		//add
+		if(cmd[0] = "add"){add(&cmd[0][0])}
+	}
+}
+*/
